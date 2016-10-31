15297ecddb38213b3a433710348d71cc365134ed
oleksinski <oleksii.karpynskyi@mobenga.com>
2016-10-31 21:06:18 +0200

    add script and app logic to fetch difference between Instagram following and followers

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var queryString = require('query-string');
 var request = require('request');
+var fs = require('fs');
 
 var instagram = {
     client: {
@@ -11,7 +12,7 @@ var instagram = {
     },
     endpoints: {
         server: {
-            authorize: 'https://api.instagram.com/oauth/authorize/?client_id={client_id}&redirect_uri={redirect_url}&response_type=code',
+            authorize: 'https://api.instagram.com/oauth/authorize/?client_id={client_id}&redirect_uri={redirect_url}&response_type=code&scope=follower_list',
             access_token: 'https://api.instagram.com/oauth/access_token?client_id={client_id}&client_secret={client_secret}&grant_type=authorization_code&redirect_uri={redirect_url}&code={code}'
         },
         client: {
@@ -104,6 +105,62 @@ app.get('/redirect', function (req, res) {
 
 });
 
+// === Proceed files /data/followers.json & /data/following.json === //
+
+app.get('/follow/single/:p', function (req, res) {
+    var p = req.params.p;
+    readJSONFile(__dirname + '/data/' + p + '.json', function (err, json) {
+        if (err) {
+            jsonPrint(res, err);
+        } else {
+            var users = {};
+            var size = 0;
+            for(var i in json) {
+                if (json.hasOwnProperty(i)) {
+                    users[i] = json[i];
+                    size++;
+                }
+            }
+            jsonPrint(res, {
+                title: p,
+                total: size,
+                users: json
+            });
+        }
+    });
+});
+
+// :following/:followers
+app.get('/follow/multi/:p1/:p2', function(req, res) {
+    var p1 = req.params.p1;
+    var p2 = req.params.p2;
+    readJSONFile(__dirname + '/data/' + p1 + '.json', function (err, json1) {
+        if (err) {
+            jsonPrint(res, err);
+        } else {
+            readJSONFile(__dirname + '/data/' + p2 + '.json', function (err, json2) {
+                if (err) {
+                    jsonPrint(res, err);
+                } else {
+                    var json = {};
+                    var size = 0;
+                    for(var i in json1) {
+                        if (json1.hasOwnProperty(i) && !json2.hasOwnProperty(i)) {
+                            json[i] = json1[i];
+                            size++;
+                        }
+                    }
+                    jsonPrint(res, {
+                        title: p1 + ' - ' + p2,
+                        total: size,
+                        users: json
+                    });
+                }
+            });
+        }
+    });
+});
+
 app.listen(8081, function () {});
 
 // ----
@@ -126,4 +183,18 @@ function getServerAccessTokenUrl(code) {
 
 function jsonPrint(res, json) {
     res.end(JSON.stringify(json));
+}
+
+function readJSONFile(filename, callback) {
+    fs.readFile(filename, function (err, data) {
+        if(err) {
+            callback(err);
+            return;
+        }
+        try {
+            callback(null, JSON.parse(data));
+        } catch(exception) {
+            callback(exception);
+        }
+    });
 }
---
+++ scripts/followers.js
@@ -0,0 +1,52 @@
+/**
+ * Grab all followers:
+ * -------------------
+ * 1. Goto: https://www.instagram.com/{username}/followers/
+ * 2. Scroll down all users
+ * 3. Execute the function below within the console
+ * 4. Grab output JSON and save under "/data/followers.json" file
+ *
+ * Grab all following
+ * 1. Goto: https://www.instagram.com/{username}/following/
+ * 2. Scroll down all users
+ * 3. Execute the function below within the console
+ * 4. Grab output JSON and save under "/data/following.json" file
+ */
+
+(function getUsersJSON() {
+
+    var json = {};
+
+    var classes = {
+        users: '_539vh',
+        user: '_cx1ua',
+        name: '_2uju6',
+        alias: '_4zhc5',
+        avatar: '_a012k'
+    };
+
+    var users = document.getElementsByClassName(classes.users)[0].getElementsByClassName(classes.user);
+
+    for(var i in users) {
+        var user = users[i];
+
+        if (users.hasOwnProperty(i)) {
+            var userName = user.getElementsByClassName(classes.name)[0].textContent;
+            //console.log(userName);
+
+            var userAlias = user.getElementsByClassName(classes.alias)[0].textContent;
+            //console.log(userAlias);
+
+            var userAvatar = user.getElementsByClassName(classes.avatar)[0].src;
+            //console.log(userAvatar);
+
+            json[userAlias] = {
+                alias: userAlias,
+                name: userName,
+                avatar: userAvatar
+            };
+        }
+    }
+
+    console.log(JSON.stringify(json));
+})();
